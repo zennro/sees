d6d626143813b541f689d36df424b2ce3e44714f
Gökhan ALKAN <galkan@outlook.com>
2014-03-06 08:37:48 +0200

    Update version.py

--- lib/version.py
+++ lib/version.py
@@ -1,6 +1,4 @@
-site = "http://www.galkan.net/"
-author = "Galkan - %s"% site
 version = "1.0"
-message = "\nSEES v%s - %s"% (version, author)
+message = "\nSEES"%
 disclamer = "Using SEES for malicious purposes is illegal. USE AT YOUR OWN RISK, Agree (Y|n)"
 wrong_option = "Wrong option, Please use \"Y|N\""
