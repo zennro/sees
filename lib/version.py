172811b7e0123cc0c72b30878cf4b7dd6c5f8ae1
Gökhan ALKAN <galkan@outlook.com>
2014-03-06 08:39:03 +0200

    Update version.py

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
 version = "1.0"
-message = "\nSEES"%
+message = "\nSEES"
 disclamer = "Using SEES for malicious purposes is illegal. USE AT YOUR OWN RISK, Agree (Y|n)"
 wrong_option = "Wrong option, Please use \"Y|N\""
